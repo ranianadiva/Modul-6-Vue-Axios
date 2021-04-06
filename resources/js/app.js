b88ee9317efdb371aad803719c2a59ee715f08a6
ranianadiva <ranianadiva11@gmail.com>
2021-04-06 10:52:36 +0700

    modul 6 vue axios

---
+++ resources/js/app.js
@@ -0,0 +1,27 @@
+require('./bootstrap');
+window.Vue = require('vue').default;
+
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import { routes } from "./routes.js";
+import Vue from 'vue';
+
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+const router = new VueRouter({
+    routes : routes,
+    mode : 'history'
+})
+
+const app = new Vue({
+    el: '#app',
+    router : router,
+    render : h => h(App)
+});
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
